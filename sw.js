68960cd20ccc31295106349b11ec27cec179dfbe
lazarte <75070893+Lazarteshop@users.noreply.github.com>
2026-03-12 14:39:05 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,14 @@
-const CACHE="echozone-pro-v1";
-
-const ASSETS=[
-"/",
-"/index.html",
-"/manifest.json",
-"/1768875223455.jpg"
-];
+const CACHE="echozone-pwa-v1";
 
 self.addEventListener("install",e=>{
 self.skipWaiting();
 e.waitUntil(
-caches.open(CACHE)
-.then(cache=>cache.addAll(ASSETS))
-);
-});
-self.addEventListener("activate",e=>{
-e.waitUntil(
-caches.keys().then(keys=>{
-return Promise.all(
-keys.filter(key=>key!==CACHE)
-.map(key=>caches.delete(key))
-);
-})
+caches.open(CACHE).then(cache=>cache.addAll(["/"]))
 );
 });
 
 self.addEventListener("fetch",e=>{
 e.respondWith(
-caches.match(e.request)
-.then(res=>res||fetch(e.request))
+caches.match(e.request).then(res=>res||fetch(e.request))
 );
 });
