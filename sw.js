c2a37782afa79fbe50e999deb0d4444f9c6a9ad5
lazarte <75070893+Lazarteshop@users.noreply.github.com>
2026-04-30 22:23:02 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,3 @@
-const CACHE="echozone-pwa-v1";
-
-self.addEventListener("install",e=>{
-self.skipWaiting();
-e.waitUntil(
-caches.open(CACHE).then(cache=>cache.addAll(["/"]))
-);
-});
-
-self.addEventListener("fetch",e=>{
-e.respondWith(
-caches.match(e.request).then(res=>res||fetch(e.request))
-);
-});
+self.addEventListener('install', e => {
+  console.log('SW installed')
+})
